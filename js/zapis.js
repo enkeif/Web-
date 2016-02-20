4e22addb8818204ff4fae64a87b60756c4914f78
Nikolina Doycheva <enkeif@abv.bg>
2016-02-20 19:46:48 +0200

    Added files via upload

---
+++ js/zapis.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+     $("#lesson2, #lesson1, #lesson3, #lesson4, #lesson5, #lesson6, #lesson7, #lesson8, #lesson9").click(function(){
+       confirm("Do you want to join this class?");
+    });
+
+});
